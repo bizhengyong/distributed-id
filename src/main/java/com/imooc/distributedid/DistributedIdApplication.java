0ed50830458715518d3926fb8784d467e7ea5fc0
bizhengyong <bizhengyong_xy@163.com>
2020-11-17 20:20:29 +0800

    3-2-springboot项目使用leaf-新建springboot项目

---
+++ src/main/java/com/imooc/distributedid/DistributedIdApplication.java
@@ -0,0 +1,16 @@
+package com.imooc.distributedid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: bizy
+ * @date: 2020/11/17 20:09
+ */
+@SpringBootApplication
+public class DistributedIdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedIdApplication.class,args);
+    }
+}
